63b07ad4c0d693dc1f0ca5911ea438dd48c849d5
ajlopez <webmaster@ajlopez.com>
2014-03-02 07:07:56 -0300

    near naive implementation

--- test/near.js
+++ test/near.js
@@ -2,7 +2,7 @@
 var sgl = require('..');
 
 exports['near in empty store'] = function (test) {
-    store = sgl.createStore();
+    var store = sgl.createStore();
     
     var result = store.near(0, 0, 10);
     
@@ -11,3 +11,17 @@ exports['near in empty store'] = function (test) {
     test.equal(result.length, 0);
 };
 
+exports['near with one item'] = function (test) {
+    var store = sgl.createStore();
+    var item = { };
+    
+    store.add(0, 0, item);
+    var result = store.near(0, 0, 10);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.equal(result[0].latitude, 0);
+    test.equal(result[0].longitude, 0);
+    test.equal(result[0].item, item);
+};
